0011c57323644e351116f7ebadd7c155185fe4ee
unknown <segamegadrive153@hotmail.com>
2020-06-07 11:49:48 +0100

    timestamp reformatted, showing some light.

--- metric.py
+++ metric.py
@@ -4,7 +4,7 @@ import datetime as dt
 
 url = "http://aio-2521:8041/v1/metric/6ca53dea-a827-4205-9fcb-59b6da7c470e/measures"
 headers = {
-    'X-Auth-Token': 'gAAAAABe3CcmC6Eo6UcC4Dh-IyA-1NAcwrzf1TDK-dv0Q4x4ovNsabL_isAi40Yaj3xk6zjMjOqe7Bwwja3T_WhJDx4BNJnKT1V2mQCQGwdaf4a_aqsJb2BW22R9H38tlUG77hJkTQ1SbdH2miahoGGlzqITbp1UlXZZc8bJsZCzuCB_T3PzmBo',
+    'X-Auth-Token': 'gAAAAABe3L1fi5BzcqT753iOs_8P-RS8NjnyXmwVbDgnPIRsDTHpoFkunwmC4DRiSvOhdzhZT-UvujeOMMJpwO3gb6rvIH-aIMOYrTNmht8I3cYGR91ZL1goUHxkb3a8aqPnYaRrCCJ4GJV-gbF6wShoXJ-9mZ9rve5EvSh-sl2hVH0bFyFAiiI',
     'Content-Type': 'application/json;charset=utf-8'
 }
 response = requests.get(url, headers=headers)
@@ -15,19 +15,22 @@ if response.status_code != 200:
 else:
     print('Data retrieved below successfully:')
 
+val = 0
+
 for dataval in data:
 
     ts = dt.datetime.strptime(dataval[0][0:19], '%Y-%m-%dT%H:%M:%S')
     ts_str = str(ts)
-    ts_int = int(ts_str)
+    # ts_int = int(ts_str)
+    # print(type(ts_int))
 
     val = {
-        'timestamp': ts_int,
+        'timestamp': ts,
         'granularity': dataval[1],
         'value': dataval[2]
     }
 
-dbconn.client.write_points({"measurements": "cpu", "tags": val})
+dbconn.client.write_points([{"measurement": "cpu", "tags": val}])
 
 
 
